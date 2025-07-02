cc814370a267e9ed3e4b1392fe0d389c3156377d
AmurdAmzer <amurdamzer@gmail.com>
2025-07-01 23:43:48 -0400

    setting up firebase authentication

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -1,20 +1,15 @@
-// Import the functions you need from Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyU-B8n9o2GXHFfJGxDUfhqIsIi7WrEVw",
-  authDomain: "adamsbrain.firebaseapp.com",
-  projectId: "adamsbrain",
-  storageBucket: "adamsbrain.firebasestorage.app",
-  messagingSenderId: "402244446317",
-  appId: "1:402244446317:web:c6ba86ccdfe45bd2e3b764"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export default app;
