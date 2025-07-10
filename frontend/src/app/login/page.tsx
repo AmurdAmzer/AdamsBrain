bef299d266a1c5dd88ccba2a4437f6b4973a6138
AmurdAmzer <amurdamzer@gmail.com>
2025-07-10 13:00:06 -0400

    fix sign up in login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -81,7 +81,7 @@ export default function LoginPage() {
             href="/signup"
             className="text-indigo-600 hover:text-indigo-500"
           >
-            Don't have an account? Sign up
+            Don&quote;t have an account? Sign up
           </Link>
           </div>
         </form>
